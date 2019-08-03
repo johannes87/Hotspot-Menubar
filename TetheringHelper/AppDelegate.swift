66f07207e2f32f7a74c07d34e90c1968c5ee967e
Johannes Bittner <johannes.bittner@gmail.com>
2019-08-03 16:08:38 +0200

    Initial Commit

---
+++ TetheringHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TetheringHelper
+//
+//  Created by Johannes Bittner on 03.08.19.
+//  Copyright © 2019 Johannes Bittner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TetheringHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TetheringHelper
+//
+//  Created by Johannes Bittner on 03.08.19.
+//  Copyright © 2019 Johannes Bittner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
