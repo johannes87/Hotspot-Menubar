1186d9ad1b238d2a50d5f55fd5d9caa81de27f32
Johannes Bittner <johannes.bittner@gmail.com>
2019-11-19 15:18:35 +0100

    Add AndroidConnector.swift

---
+++ TetheringHelper/AndroidConnector.swift
@@ -0,0 +1,9 @@
+//
+//  AndroidConnector.swift
+//  TetheringHelper
+//
+//  Created by Johannes Bittner on 19.11.19.
+//  Copyright © 2019 Johannes Bittner. All rights reserved.
+//
+
+import Foundation
