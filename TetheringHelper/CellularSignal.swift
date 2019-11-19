d77f814f1a9b96f4be9ec2ae7b55e44783c226a8
Johannes Bittner <johannes.bittner@gmail.com>
2019-11-19 18:01:32 +0100

    Convert drawSignalBars/Type from closure to class method to improve readability of drawStatusItem method

--- TetheringHelper/CellularSignal.swift
+++ TetheringHelper/CellularSignal.swift
@@ -30,76 +30,76 @@ class CellularSignal {
             // https://stackoverflow.com/questions/12714923/os-x-icons-size
             let imageSize = NSSize.init(width: 18.0, height: 18.0)
 
-            let signalBarLineWidth = 1
-            let signalBarRectWidth: CGFloat = 3
-
             let statusItemImage = NSImage(
                 size: imageSize,
                 flipped: false,
                 drawingHandler: { (dstRect: NSRect) -> Bool in
-                    // TODO: figure out why some lines are drawn fuzzy
-                    func drawSignalBars() {
-                        let path = NSBezierPath()
-                        path.lineWidth = CGFloat(signalBarLineWidth)
-                        NSColor.black.setStroke()
-
-                        let signalBar1Rect = NSRect(x: NSMinX(dstRect), y: NSMinY(dstRect), width: signalBarRectWidth, height: 3)
-                        path.appendRect(signalBar1Rect)
-                        if self.signalQuality.rawValue == 1 {
-                            path.fill()
-                        }
-
-                        let signalBar2Rect = NSRect(x: NSMinX(dstRect) + signalBarRectWidth+2, y: NSMinY(dstRect), width: signalBarRectWidth, height: 6)
-                        path.appendRect(signalBar2Rect)
-                        if self.signalQuality.rawValue == 2 {
-                            path.fill()
-                        }
-
-                        let signalBar3Rect = NSRect(x: NSMinX(dstRect) + signalBarRectWidth*2+4, y: NSMinY(dstRect), width: signalBarRectWidth, height: 10)
-                        path.appendRect(signalBar3Rect)
-                        if self.signalQuality.rawValue == 3 {
-                            path.fill()
-                        }
-
-                        let signalBar4Rect = NSRect(x: NSMinX(dstRect) + signalBarRectWidth*3+6, y: NSMinY(dstRect), width: signalBarRectWidth, height: 14)
-                        path.appendRect(signalBar4Rect)
-                        if self.signalQuality.rawValue == 4 {
-                            path.fill()
-                        }
-
-                        // stroke all the appended rects
-                        path.stroke()
-                    }
-
-                    func drawSignalType() {
-                        let fontSmall = NSFont.systemFont(ofSize: 6)
-                        let fontBig = NSFont.systemFont(ofSize: 7)
-
-                        var font: NSFont
-                        if self.signalType == SignalType.lte {
-                            // the string "LTE" requires more space
-                            font = fontSmall
-                        } else {
-                            font = fontBig
-                        }
-
-                        let textFontAttributes = [
-                            NSAttributedString.Key.font: font,
-                            NSAttributedString.Key.foregroundColor: NSColor.black,
-                        ]
-
-                        let signalTypeStr = NSString(string: self.signalType.rawValue)
-                        signalTypeStr.draw(in: dstRect, withAttributes: textFontAttributes)
-                    }
-
-                    drawSignalBars()
-                    drawSignalType()
-
+                    self.drawSignalBars(dstRect)
+                    self.drawSignalType(dstRect)
                     return true
             })
 
             self.statusItem.button?.image = statusItemImage
-
         }
+    }
+
+    // Used in drawStatusItem to draw the bar shapes for the signalQuality
+    private func drawSignalBars(_ dstRect: NSRect) {
+        // TODO: figure out why some lines are drawn fuzzy
+        let signalBarLineWidth = 1
+        let signalBarRectWidth: CGFloat = 3
+
+        let path = NSBezierPath()
+        path.lineWidth = CGFloat(signalBarLineWidth)
+        NSColor.black.setStroke()
+
+        let signalBar1Rect = NSRect(x: NSMinX(dstRect), y: NSMinY(dstRect), width: signalBarRectWidth, height: 3)
+        path.appendRect(signalBar1Rect)
+        if self.signalQuality.rawValue == 1 {
+            path.fill()
+        }
+
+        let signalBar2Rect = NSRect(x: NSMinX(dstRect) + signalBarRectWidth+2, y: NSMinY(dstRect), width: signalBarRectWidth, height: 6)
+        path.appendRect(signalBar2Rect)
+        if self.signalQuality.rawValue == 2 {
+            path.fill()
+        }
+
+        let signalBar3Rect = NSRect(x: NSMinX(dstRect) + signalBarRectWidth*2+4, y: NSMinY(dstRect), width: signalBarRectWidth, height: 10)
+        path.appendRect(signalBar3Rect)
+        if self.signalQuality.rawValue == 3 {
+            path.fill()
+        }
+
+        let signalBar4Rect = NSRect(x: NSMinX(dstRect) + signalBarRectWidth*3+6, y: NSMinY(dstRect), width: signalBarRectWidth, height: 14)
+        path.appendRect(signalBar4Rect)
+        if self.signalQuality.rawValue == 4 {
+            path.fill()
+        }
+
+        // stroke all the appended rects
+        path.stroke()
+    }
+
+    // Used in drawStatusItem to draw the text for the signalType (2G, 3G, ...)
+    private func drawSignalType(_ dstRect: NSRect) {
+        let fontSmall = NSFont.systemFont(ofSize: 6)
+        let fontBig = NSFont.systemFont(ofSize: 7)
+
+        var font: NSFont
+        if self.signalType == SignalType.lte {
+            // the string "LTE" requires more space
+            font = fontSmall
+        } else {
+            font = fontBig
+        }
+
+        let textFontAttributes = [
+            NSAttributedString.Key.font: font,
+            NSAttributedString.Key.foregroundColor: NSColor.black,
+        ]
+
+        let signalTypeStr = NSString(string: self.signalType.rawValue)
+        signalTypeStr.draw(in: dstRect, withAttributes: textFontAttributes)
     }
 }
