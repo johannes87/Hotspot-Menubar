94adbf57116bb8a59cfbd5f885d21b4dba2e060b
Johannes Bittner <johannes.bittner@gmail.com>
2019-11-29 00:52:05 +0100

    encapsulate status item menu code into class

---
+++ TetheringHelper/SignalStatusItemMenu.swift
@@ -0,0 +1,57 @@
+//
+//  SignalStatusItemMenu.swift
+//  TetheringHelper
+//
+//  Created by Johannes Bittner on 29.11.19.
+//  Copyright © 2019 Johannes Bittner. All rights reserved.
+//
+
+import Cocoa
+
+class SignalStatusItemMenu {
+    var menu: NSMenu
+
+    private var dataUsedMB = 0.0
+    private static let dataStatisticsMenuItemTitle = NSLocalizedString(
+        "Data used: %.2f MB",
+        comment: "amount of data used, shown in status item menu")
+
+    private var dataStatisticsMenuItem = NSMenuItem(
+        title: String(format: dataStatisticsMenuItemTitle, 0.0),
+        action: #selector(showDataStatistics(sender:)),
+        keyEquivalent: "")
+
+    private static let pairMenuItemUnpairedTitle = NSLocalizedString(
+        "Pair with phone...",
+        comment: "status item menu item title when phone is not paired yet")
+
+    private var pairMenuItem = NSMenuItem(
+        title: pairMenuItemUnpairedTitle,
+        action: #selector(pairPhone(sender:)),
+        keyEquivalent: "")
+
+    init() {
+        menu = NSMenu(title: "Some title, where is it?") // TODO: find out where it is
+        menu.autoenablesItems = false
+
+        dataStatisticsMenuItem.target = self
+        menu.insertItem(dataStatisticsMenuItem, at: 0)
+
+        menu.insertItem(NSMenuItem.separator(), at: 1)
+
+        pairMenuItem.target = self
+        menu.insertItem(pairMenuItem, at: 2)
+    }
+
+    @IBAction private func showDataStatistics(sender: Any) {
+        print("show data statistics")
+        dataUsedMB += 1.0 // TODO remove
+        dataStatisticsMenuItem.title = String(
+            format: SignalStatusItemMenu.dataStatisticsMenuItemTitle,
+            dataUsedMB)
+    }
+
+    @IBAction private func pairPhone(sender: Any) {
+        print("pair phone action")
+    }
+}
