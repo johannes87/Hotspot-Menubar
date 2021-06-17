4f18404f750d7c128495b58e54f0d5ad6cf67a59
Johannes Bittner <johannes.bittner@gmail.com>
2021-06-17 13:54:32 +0200

    values can be const, avoid warning

--- Android/app/src/main/java/com/gmail/bittner/johannes/tetheringhelper/SharedPreferencesKeys.kt
+++ Android/app/src/main/java/com/gmail/bittner/johannes/tetheringhelper/SharedPreferencesKeys.kt
@@ -2,7 +2,7 @@ package com.gmail.bittner.johannes.tetheringhelper
 
 class SharedPreferencesKeys {
     companion object Keys {
-        val phoneName = "phone_name"
-        val firstTimeSetupFinished = "first_time_setup_finished"
+        const val phoneName = "phone_name"
+        const val firstTimeSetupFinished = "first_time_setup_finished"
     }
 }
