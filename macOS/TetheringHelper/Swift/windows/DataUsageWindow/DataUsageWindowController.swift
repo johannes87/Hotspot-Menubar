3ee26dac5483ab9bf9d8e32a97e88f5c1372f045
Johannes Bittner <johannes.bittner@gmail.com>
2021-10-22 14:50:46 +0200

    if the "no data recorded" info is shown, automatically replace with data usage display once a session gets created

--- macOS/TetheringHelper/Swift/windows/DataUsageWindow/DataUsageWindowController.swift
+++ macOS/TetheringHelper/Swift/windows/DataUsageWindow/DataUsageWindowController.swift
@@ -11,6 +11,8 @@ class DataUsageWindowController: FrontCenterWindowController {
     
     private var noSessionsViewController: NSViewController!
     private var hasSessionsViewController: NSViewController!
+    
+    private var sessionChangeObserver: Any?
 
     override func windowDidLoad() {
         super.windowDidLoad()
@@ -18,6 +20,15 @@ class DataUsageWindowController: FrontCenterWindowController {
             .instantiateController(withIdentifier: "noSessions") as? NSViewController
         hasSessionsViewController = NSStoryboard(name: "DataUsageWindow", bundle: nil)
             .instantiateController(withIdentifier: "hasSessions") as? DataUsageViewController
+        
+        // make window automatically replace its "noSession" view controller when a session gets created
+        if PersistentContainer.shared.getTetheringSessions().count == 0 {
+            sessionChangeObserver = PersistentContainer.observeSessionChanges { [unowned self] _ in
+                self.contentViewController = self.hasSessionsViewController
+                self.window?.center()
+                NotificationCenter.default.removeObserver(self.sessionChangeObserver!)
+            }
+        }
     }
     
     override func showWindow(_ sender: Any?) {
