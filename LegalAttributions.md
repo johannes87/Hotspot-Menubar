a6f78af40884c2a8f0df70190bb7564e1cdd0b17
Johannes Bittner <johannes.bittner@gmail.com>
2021-11-19 14:01:47 +0100

    Make status item height smaller, to align with other icons (e.g. mac wifi icon)
    - Delete unpairedIcon, no longer necessary
    - Draw a line to "strike through" the bars, to show we're disconnected
      from the phone; unpairedIcon was now too small, and this way it's also
      similar to how the mac wifi icon
    - make inactive bar more easily visible (increase opacity)
    - draw signal type next to signal-strength-bars, because it didn't fit
      in the top-left corner anymore; therefore, the width of the status item
      is now dynamic.

--- LegalAttributions.md
+++ LegalAttributions.md
@@ -1,7 +1,5 @@
 # macOS app
 
-Disconnected icon by Icons8: https://icons8.com/icon/rkNtl8nOsiha/disconnected
-
 Google Play and the Google Play logo are trademarks of Google LLC.
 
 # Android app
--- macOS/Hotspot Menubar/Swift/statusitem/StatusItem.swift
+++ macOS/Hotspot Menubar/Swift/statusitem/StatusItem.swift
@@ -9,9 +9,22 @@
 import Cocoa
 
 class StatusItem: StatusItemDelegate {
+    private static let statusItemLengthUnpaired = 18.0
+
     private(set) var statusItemMenu: StatusItemMenu!
 
     private let cocoaStatusItem: NSStatusItem
+    private var statusItemLength: CGFloat {
+        get {
+            if pairingStatus.isPaired {
+                // the final length depends on the width of the signal type string
+                let signalType = getAttributedStringForSignalType()
+                return StatusItem.statusItemLengthUnpaired + signalType.size().width
+            } else {
+                return StatusItem.statusItemLengthUnpaired
+            }
+        }
+    }
 
     private var signalQuality = SignalQuality.no_signal
     private var signalType = SignalType.no_signal
@@ -20,7 +33,7 @@ class StatusItem: StatusItemDelegate {
 
     init() {
         statusItemMenu = StatusItemMenu()
-        cocoaStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        cocoaStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         cocoaStatusItem.menu = statusItemMenu.menu
 
         self.drawStatusItem()
@@ -28,8 +41,10 @@ class StatusItem: StatusItemDelegate {
 
     private func drawStatusItem() {
         DispatchQueue.main.async {
-            // choosing 18/18: https://stackoverflow.com/questions/12714923/os-x-icons-size
-            let imageSize = NSSize.init(width: 18.0, height: 18.0)
+            // choosing 18 height: https://stackoverflow.com/questions/12714923/os-x-icons-size
+            let imageSize = NSSize.init(
+                width: self.statusItemLength, height: 18.0
+            )
 
             let statusItemImage = NSImage(
                 size: imageSize,
@@ -39,7 +54,7 @@ class StatusItem: StatusItemDelegate {
                     if self.pairingStatus.isPaired {
                         self.drawSignalType(dstRect)
                     } else {
-                        self.drawUnpairedIcon(dstRect)
+                        self.drawDisconnectedStrikeThrough(dstRect)
                     }
                     return true
             }
@@ -52,41 +67,44 @@ class StatusItem: StatusItemDelegate {
     private func drawSignalBars(_ dstRect: NSRect) {
         let signalBarRectWidth: CGFloat = 3
 
+        let xOffset = 0.0
+        let yOffset = 3.0
+
         let signalBar1Rect = NSRect(
-            x: NSMinX(dstRect),
-            y: NSMinY(dstRect),
+            x: NSMinX(dstRect) + xOffset,
+            y: NSMinY(dstRect) + yOffset,
             width: signalBarRectWidth,
-            height: 3
+            height: 4
         )
         drawSignalBar(signalBarRect: signalBar1Rect, minActiveQuality: 1)
 
         let signalBar2Rect = NSRect(
-            x: NSMinX(dstRect) + signalBarRectWidth + 2,
-            y: NSMinY(dstRect),
+            x: NSMinX(dstRect) + signalBarRectWidth + 1 + xOffset,
+            y: NSMinY(dstRect) + yOffset,
             width: signalBarRectWidth,
             height: 6
         )
         drawSignalBar(signalBarRect: signalBar2Rect, minActiveQuality: 2)
 
         let signalBar3Rect = NSRect(
-            x: NSMinX(dstRect) + signalBarRectWidth * 2 + 4,
-            y: NSMinY(dstRect),
+            x: NSMinX(dstRect) + signalBarRectWidth * 2 + 2 + xOffset,
+            y: NSMinY(dstRect) + yOffset,
             width: signalBarRectWidth,
-            height: 10
+            height: 9
         )
         drawSignalBar(signalBarRect: signalBar3Rect, minActiveQuality: 3)
 
         let signalBar4Rect = NSRect(
-            x: NSMinX(dstRect) + signalBarRectWidth * 3 + 6,
-            y: NSMinY(dstRect),
+            x: NSMinX(dstRect) + signalBarRectWidth * 3 + 3 + xOffset,
+            y: NSMinY(dstRect) + yOffset,
             width: signalBarRectWidth,
-            height: 14
+            height: 12
         )
         drawSignalBar(signalBarRect: signalBar4Rect, minActiveQuality: 4)
     }
 
     private func drawSignalBar(signalBarRect rect: NSRect, minActiveQuality: Int) {
-        let alphaBarInactive: CGFloat = 0.2
+        let alphaBarInactive: CGFloat = 0.25
         let alphaBarActive: CGFloat = 1
 
         if self.signalQuality.rawValue >= minActiveQuality {
@@ -102,39 +120,44 @@ class StatusItem: StatusItemDelegate {
 
     /// Used in drawStatusItem to draw the text for the signalType (2G, 3G, ...)
     private func drawSignalType(_ dstRect: NSRect) {
-        let fontSmall = NSFont.systemFont(ofSize: 6)
-        let fontBig = NSFont.systemFont(ofSize: 7)
+        let xOffset = 18.0
+        let yOffset = -3.0
 
-        var font: NSFont
-        if self.signalType == .lte || self.signalType == .five_g_plus {
-            // some signal types require more space
-            font = fontSmall
-        } else {
-            font = fontBig
-        }
-
-        let signalTypeStr = NSString(string: self.signalType.rawValue)
-        signalTypeStr.draw(
-            in: dstRect,
-            withAttributes: [.font: font, .foregroundColor: NSColor.labelColor]
+        let signalType = getAttributedStringForSignalType()
+        signalType.draw(
+            in: NSRect(
+                x: dstRect.origin.x + xOffset,
+                y: dstRect.origin.y + yOffset,
+                width: dstRect.width,
+                height: dstRect.height
+            )
         )
     }
 
-    /// Draw an "unpaired" icon in the top-left corner when this app is not paired with an Android device
-    private func drawUnpairedIcon(_ dstRect: NSRect) {
-        // TODO: fulfill icon license http://www.iconarchive.com/show/windows-8-icons-by-icons8/Network-Disconnected-icon.html
-        // TODO: draw icon again as vector image, by backdropping the bitmap and drawing on top
-        let unpairedIcon = NSImage(named: "unpairedIcon")!
-        guard dstRect.width == 18 && dstRect.height == 18 else { return }
+    /// - Returns: the NSAttributedString that is going to be drawn into the status item
+    private func getAttributedStringForSignalType() -> NSAttributedString {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: NSFont.systemFont(ofSize: 10),
+            .foregroundColor: NSColor.labelColor
+        ]
 
-        let opacity = 0.3
+        return NSAttributedString(
+            string: signalType.rawValue,
+            attributes: attributes
+        )
+    }
 
-        // the destination is assumed to be 18x18 px here
-        unpairedIcon.draw(
-            in: NSRect(x: 0, y: 8, width: 10, height: 10),
-            from: .zero,
-            operation: .sourceOver,
-            fraction: opacity)
+    /// This draws a line that "strikes through" the signal bars, to communicate there's no connection with the phone.
+    private func drawDisconnectedStrikeThrough(_ dstRect: NSRect) {
+        let xOffset = 2
+        let yOffset = 3
+
+        let path = NSBezierPath()
+        // A line with slope = -1
+        path.move(to: .init(x: 12 + xOffset, y: 0 + yOffset))
+        path.line(to: .init(x: 0 + xOffset, y: 12 + yOffset))
+        NSColor.labelColor.setStroke()
+        path.stroke()
     }
 
     // MARK: StatusItemDelegate
